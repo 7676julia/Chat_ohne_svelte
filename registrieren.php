45f65d98ba941540a19bb3bf1415cf4279e44c22
MeDeeg <melinadeeg@gmail.com>
2024-12-11 11:24:15 +0100

    reigister v1

--- registrieren.php
+++ registrieren.php
@@ -1,3 +1,31 @@
+<?php
+require("start.php");
+
+$error = ""; 
+$username = $_POST['username'];
+$password = $_POST['password'];
+$confirm = $_POST['confirm'];
+
+if(strlen($username) < 3 ){
+    $error = "Der Nutzername muss mindestens 3 Zeichen lang sein.";
+}elseif (strlen($password) < 8){
+    $error = "Das Passwort muss mindestens 8 Zeichen lang sein.";
+}elseif ($password !== $confirm) {
+    $error = "Die Passwörter stimmen nicht überein.";
+}elseif (BackendService::loadUser($username)) { 
+    $error = "Der Nutzername ist bereits vergeben.";
+}else {
+    // Registrierung ausführen
+    if (BackendService::register($username, $password)) {
+        $_SESSION['user'] = $username;
+        header("Location: login.php");
+        exit();
+    } else {
+        $error = "Die Registrierung ist fehlgeschlagen.";
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,6 +40,11 @@
 <body>
     <img src="images/user.png" style="height: 100px">
     <h1>Register yourself</h1>
+
+    <?php if ($error): ?>
+        <p style="color: red;"><?php echo htmlspecialchars($error); ?></p>
+    <?php endif; ?>
+    
     <form action="freundeliste.php" method="get" id="registerForm">
         <fieldset>
             <div class="field">
