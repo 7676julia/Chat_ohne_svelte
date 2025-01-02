ab2827f2ce9b0508bd4eec8c69c1f66fd7e05ab4
MeDeeg <melinadeeg@gmail.com>
2025-01-02 16:16:15 +0100

    register korriegiert

--- login.php
+++ login.php
@@ -38,9 +38,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login - Chat Application</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="app.css">
 </head>
 <body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <img src="images/chat.png" alt="Chat Logo" style="height: 100px">
     <h1>Please sign in</h1>
     
